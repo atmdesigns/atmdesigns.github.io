00f7ea14d21f23794dd24a1d8ef1f0f855a47bd7
The Floods <thefloods@Flood-MacBook.attlocal.net>
2015-04-30 19:48:17 -0400

    correct 2

--- views/js/main.js
+++ views/js/main.js
@@ -507,10 +507,10 @@ function updatePositions() {
   
 
   //Moved all non changing items below outside of for loop
-   var phase = Math.sin((document.body.scrollTop / 1250));  
+   
   for (var i = 0; i < items.length; i++) {
     phase = phase + (i % 5);
-    
+    var phase = Math.sin((document.body.scrollTop / 1250));  
     //Let's log these numbers to see exactly what I get per iteration?
     //console.log(phase,document.body.scrollTop / 1250)
    
