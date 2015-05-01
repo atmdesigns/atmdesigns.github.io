66a876df40fc080de47a171bf9ebc5f99620ee0b
The Floods <thefloods@Flood-MacBook.attlocal.net>
2015-04-30 22:16:29 -0400

    another test

--- views/js/main.js
+++ views/js/main.js
@@ -455,12 +455,14 @@ var resizePizzas = function(size) {
 
     //changed querySelectorAll to getElementsByClass name as it is more efficient
     for (var i = 0; i < myContainer.length; i++) {
+      var myItem = myContainer[i];
+
       //var dx = determineDx(document.getElementsByClassName("randomPizzaContainer")[i], size);
       //var newwidth = (document.getElementsByClassName("randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       //document.getElementsByClassName("randomPizzaContainer")[i].style.width = newwidth;
-      var dx = determineDx(myContainer[i], size);
-      var newwidth = (myContainer[i].offsetWidth + dx) + 'px';
-      myContainer[i].style.width = newwidth;
+      var dx = determineDx(myItem, size);
+      var newwidth = (myItem.offsetWidth + dx) + 'px';
+      myItem.style.width = newwidth;
     }
   }
 
